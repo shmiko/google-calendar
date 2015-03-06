44df6cc2a83804a66877560a00594e2d88bd7ea6
Paul Jones <shmiko@gmail.com>
2015-03-06 21:28:15 +1100

    cal keys out

--- specs/config.js
+++ specs/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 		
-	 consumer_key		  : '407717796667-7euei2g372vo63u7blvkl1pcb5vvsald.apps.googleusercontent.com',
-   consumer_secret  : '-sF3T1tNgIdIprzEHFQZzfdh',
+	 consumer_key		  : '',
+   consumer_secret  : '',
    refresh_token    : 'REFEASH_TOKEN',
 }
