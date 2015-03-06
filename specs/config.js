de921f64b9ddf2799cb8396bd6aa00e035b0fef0
Paul Jones <shmiko@gmail.com>
2015-03-06 21:27:31 +1100

    gcal node

--- specs/config.js
+++ specs/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 		
-	 consumer_key		  : 'CONSUMER_KEY',
-   consumer_secret  : 'CONSUMER_SECRET',
+	 consumer_key		  : '407717796667-7euei2g372vo63u7blvkl1pcb5vvsald.apps.googleusercontent.com',
+   consumer_secret  : '-sF3T1tNgIdIprzEHFQZzfdh',
    refresh_token    : 'REFEASH_TOKEN',
 }
